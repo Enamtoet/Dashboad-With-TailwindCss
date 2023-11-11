cd7440d147f1ceae2a6849d0b68f4820b282f2f8
enamtoet <otmane.ettalabi1@gmail.com>
2023-11-11 02:15:31 +0100

    Add Responsive To Sidbar Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,19 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
+      // // Add Screen Sizes
+      // screens: {
+      //   'sm': '768px',   // Small screens
+      //   'md': '992px',   // Medium screens
+      //   'lg': '1200px',  // Large screens
+      //   'xl': '1440px',  // Extra-large screens
+      // },
+      // Add Colors 
       colors:{
         bgray:"#f1f9f5",
         backgroundColor: "#f6f6f6"
       },
+      // Add Fonts 
       fontFamily:{
         cairo:['Open Sans', 'Cairo', "sans-serif"]
       }
